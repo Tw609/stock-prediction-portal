93c48062d1d750c2a8200a00b35f5b123d7a6d01
Tw609 <channboth1233@gmail.com>
2026-05-13 21:37:27 +0700

    Login functionally

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./assets/css/style.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -6,19 +5,22 @@ import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import AuthProvider from "./components/AuthProvider";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthProvider";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoding] = useState(false);
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoding(true);
+
+    const userData = { username, password };
+    console.log("userData==>", userData);
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/token/",
+        userData,
+      );
+      localStorage.setItem("accessToken", response.data.access);
+      localStorage.setItem("refreshToken", response.data.refresh);
+      console.log("Login Successfull");
+      setIsLoggedIn(true);
+      navigate("/");
+    } catch {
+      console.error("Invalid credentials");
+      setError("Invalid Credential");
+    } finally {
+      setLoding(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 bg-light-dark p-5 rounded">
+            <h3 className="text-light text-center mb-4">Login to our Portal</h3>
+            <form onSubmit={handleLogin}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="form-control"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="Password"
+                  placeholder="Password"
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              {error && <div className="text-danger">{error}</div>}
+              {loading ? (
+                <button
+                  type="submit"
+                  className="btn btn-info d-block mx-auto"
+                  disabled>
+                  <FontAwesomeIcon icon={faSpinner} spin />
+                  Loggin in...
+                </button>
+              ) : (
+                <button type="submit" className="btn btn-info d-block mx-auto">
+                  Login
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
